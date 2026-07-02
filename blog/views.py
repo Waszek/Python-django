337c12035f76f12df03ea543ead474c8a6abe7f0
Krzysztof W <waszur20@gmail.com>
2026-07-02 19:56:41 +0200

    view user form database on HTML page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    all_posts = Post.objects.all()
+    context = {'posts': all_posts}
+    return render(request, 'blog/index.html', context)
