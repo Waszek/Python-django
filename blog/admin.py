1d4e329f367da64cea7fbbf81e9d427b3006711f
Krzysztof W <waszur20@gmail.com>
2026-07-02 19:35:03 +0200

    made migrations and add users and post to database

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import BlogUser, Post
+
+admin.site.register(BlogUser)
+admin.site.register(Post)
+# Register your models here.
---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+#Table for users
+class BlogUser(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+
+#Table for posts with foreign key
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(BlogUser, on_delete=models.CASCADE)
