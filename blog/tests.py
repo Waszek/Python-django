89e9c079a81c58e62700863a55c4ad19377193e5
Krzysztof W <waszur20@gmail.com>
2026-07-06 20:08:43 +0200

    added test to blog app

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import BlogUser, Post
 
-# Create your tests here.
+class BlogViewTests(TestCase):
+
+    def setUp(self):
+        self.user = BlogUser.objects.create(name="Tester Jan", company="CORPORATION")
+        self.post = Post.objects.create(title="Test record", user=self.user)
+
+    def test_blog_index_view_status_code(self):
+        response = self.client.get(reverse('index'))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_index_view_displays_posts(self):
+        response = self.client.get(reverse('index'))
+
+        self.assertContains(response, "Test record")
+        self.assertContains(response, "Tester Jan")
